3590808b8fcc7dc92f192156f68ab5df7331add5
EscAlice <chenxianjie@icarephone.com>
2020-07-20 10:48:30 +0800

    feat:新增创建数据库逻辑

--- db/db.go
+++ db/db.go
@@ -1,23 +1,47 @@
 package db
 
 import (
+	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 )
+
+const DatabaseName string = "demo_order"
 
 // 初始化数据库连接
 func Init() (*gorm.DB, error) {
 
 	var Db *gorm.DB
 	var err error
-	Db, err = gorm.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/demo_order?charset=utf8&parseTime=True&loc=Local&timeout=10ms")
 
+	// 连接数据库
+	Db, err = gorm.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/"+DatabaseName+"?charset=utf8&parseTime=True&loc=Local&timeout=10ms")
 	if err != nil {
-		return nil, err
-	}
-
-	if Db.Error != nil {
-		return nil, err
+		// 如果数据库不存在则创建数据库
+		createDatabase(DatabaseName)
 	}
 	return Db, nil
+}
+
+// 创建数据库
+func createDatabase(name string) {
+
+	// 连接
+	db, err := sql.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// 创建数据库
+	_, err = db.Exec("CREATE DATABASE " + name)
+	if err != nil {
+		panic(err)
+	}
+
+	// 数据库使用权限
+	_, err = db.Exec("USE " + name)
+	if err != nil {
+		panic(err)
+	}
 }
